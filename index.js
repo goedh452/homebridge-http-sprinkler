fb5934d515ea972e4b21166abc617739d25afd58
goedh452 <bg.goedhart@gmail.com>
2018-05-12 21:55:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -88,8 +88,8 @@ function HttpSprinkler(log, config) {
 //                that.state = re.test(data);
 		    
 	    var json = JSON.parse(data);
-	    that.log("Waarde status: " + data); //json.result.status);
-            that.state = json.result.status;
+	    that.log("Waarde status: " + json.result.Status);
+            that.state = json.result.Status;
 		    
             }
             else {
