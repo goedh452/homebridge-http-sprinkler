138efb18f58077ad7e432b210e64f70f13f99233
goedh452 <bg.goedhart@gmail.com>
2018-05-12 19:29:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,135 @@
+"use strict";
+
 var Service, Characteristic;
-var request = require('sync-request');
+var request = require("request");
+var pollingtoevent = require('polling-to-event');
 
-var url 
 
-module.exports = function (homebridge) {
+module.exports = function(homebridge) {
+
     Service = homebridge.hap.Service;
     Characteristic = homebridge.hap.Characteristic;
-    homebridge.registerAccessory("homebridge-http-sprinkler", "HttpSprinkler", HttpSprinkler);
-}
+
+    homebridge.registerAccessory("homebridge-http-sprinkler", "http-sprinkler", HttpSprinkler);
+};
 
 
 function HttpSprinkler(log, config) {
-    	this.log = log;
+    this.log = log;
 
-    	// url info
-	this.on_url 		= config["on_url"];
-    	this.off_url 		= config["off_url"];
-    	this.status_url 	= config["status_url"];
-	this.status_regex 	= config["status_regex"];
-    	this.http_method 	= config["http_method"];
-    	this.sendimmediately 	= config["sendimmediately"];
-    	this.default_state_off 	= config["default_state_off"];
-    	this.name 		= config["name"];
+    this.name                   = config["name"]                || "HTTP Switch";
+    this.checkStatus 	        = config["checkStatus"] 	|| "no";
+    this.pollingMillis          = config["pollingMillis"]       || 10000;
+    this.onUrl                  = config["onUrl"];
+    this.onBody                 = config["onBody"];
+    this.offUrl                 = config["offUrl"];
+    this.offBody                = config["offBody"];
+    this.statusUrl              = config["statusUrl"];
+    this.statusRegex            = config["statusRegex"]		|| "";
+    this.httpMethod             = config["httpMethod"] 	  	|| "GET";
+
+    this.state = false;
+
+    var that = this;
+
+    this.services = {
+        AccessoryInformation: new Service.AccessoryInformation(),
+        Switch: new Service.Switch(this.name)
+    };
+
+    this.services.AccessoryInformation
+        .setCharacteristic(Characteristic.Manufacturer, "vectronic");
+    this.services.AccessoryInformation
+        .setCharacteristic(Characteristic.Model, "HTTP Switch");
+
+    switch (this.checkStatus) {
+        case "yes":
+            this.services.Switch
+                .getCharacteristic(Characteristic.On)
+                .on('get', this.getStatusState.bind(this))
+                .on('set', this.setPowerState.bind(this));
+            break;
+        case "polling":
+            this.services.Switch
+                .getCharacteristic(Characteristic.On)
+                .on('get', function(callback) {callback(null, that.state)})
+                .on('set', this.setPowerState.bind(this));
+            break;
+        default	:
+            this.services.Switch
+                .getCharacteristic(Characteristic.On)
+                .on('set', this.setPowerState.bind(this));
+            break;
+    }
+
+    // Status Polling
+    if (this.statusUrl && this.checkStatus === "polling") {
+
+        var url = this.statusUrl;
+        var statusemitter = pollingtoevent(function(done) {
+            that.httpRequest(url, "", "GET", function(error, response, body) {
+                if (error) {
+                    that.log('HTTP get status function failed: %s', error.message);
+                    callback(error);
+                }
+                else {
+                    done(null, body);
+                }
+            })
+        }, {longpolling:true, interval:that.pollingMillis, longpollEventName:"statuspoll"});
+
+        statusemitter.on("statuspoll", function(data) {
+            if (Boolean(that.statusRegex)) {
+                var re = new RegExp(that.statusRegex);
+                that.state = re.test(data);
+            }
+            else {
+                var binaryState = parseInt(data);
+                that.state = binaryState > 0;
+            }
+            that.log("status received from: " + that.statusUrl, "state is currently: ", that.state.toString());
+
+            that.services.Switch
+                .getCharacteristic(Characteristic.On)
+                .setValue(that.state);
+        });
+    }
 }
 
-HttpSprinkler.prototype = {
 
-    httpRequest: function (url, body, method, username, password, sendimmediately, callback) {
-        request({
-                    url: url,
-                    body: body,
-                    method: method,
-                    rejectUnauthorized: false
-                },
-                function (error, response, body) {
-                    callback(error, response, body)
-                })
-    },
+HttpSwitchAccessory.prototype.httpRequest = function (url, body, method, callback) {
 
-    getPowerState: function (callback) {
-        if (!this.status_url) {
+    var callbackMethod = callback;
+
+    request({
+            url: url,
+            body: body,
+            method: method,
+            rejectUnauthorized: false
+        },
+        function (error, response, responseBody) {
+            if (callbackMethod) {
+                callbackMethod(error, response, responseBody)
+            }
+            else {
+                this.log.warn("callbackMethod not defined!");
+            }
+        })
+};
+
+
+HttpSwitchAccessory.prototype.getStatusState = function (callback) {
+
+    if (!this.statusUrl) {
         this.log.warn("Ignoring request: No status url defined.");
         callback(new Error("No status url defined."));
         return;
     }
 
-    var url = this.status_url;
+    var url = this.statusUrl;
     var regex = this.statusRegex;
 
-    this.httpRequest(url, "", "GET", "", "", "", function (error, response, responseBody) {
+    this.httpRequest(url, "", "GET", function (error, response, responseBody) {
         if (error) {
             this.log('HTTP get status function failed: %s', error.message);
             callback(error);
@@ -66,54 +147,44 @@ HttpSprinkler.prototype = {
             this.log("status received from: " + url, "state is currently: ", powerOn.toString());
             callback(null, powerOn);
         }
-    },
+    }.bind(this));
+};
 
-    setPowerState: function(powerOn, callback) {
-        var body;
-	var url;
-	    
-	if (powerOn) {
-        	url = this.on_url;
-        	this.log("Setting power state to on");
-        } else {
-        	url = this.off_url;
-        	this.log("Setting power state to off");
-        }
 
-		var res = request(this.http_method, this.url, {});
-		if(res.statusCode > 400){
-			this.log('HTTP power function failed');
-			callback(error);
-		}else{
-			this.log('HTTP power function succeeded!');
-            var info = JSON.parse(res.body);
-            this.log(res.body);
-            this.log(info);
-			callback();
-		}
+HttpSwitchAccessory.prototype.setPowerState = function (powerOn, callback) {
 
-    },
+    var url;
+    var body;
 
-    identify: function (callback) {
-        this.log("Identify requested!");
-        callback(); // success
-    },
-
-    getServices: function () {
-	var valveService = new Service.Valve();
-	    
-	valveService.isPrimaryService = true;
-	valveService.displayName = "Service.Valve";
-	valveService.timer = null;
-			
-	valveService.getCharacteristic(Characteristic.Active)
-		.on('set', this.setPowerState.bind(this))
-					
-	valveService.getCharacteristic(Characteristic.InUse)
-					
-	valveService.getCharacteristic(Characteristic.ValveType)
-		.updateValue(1)
-	       
-        return [valveService];
+    if (!this.onUrl || !this.offUrl) {
+        this.log.warn("Ignoring request: No power url defined.");
+        callback(new Error("No power url defined."));
+        return;
     }
+
+    if (powerOn) {
+        url = this.onUrl;
+        body = this.onBody;
+        this.log("Setting power state to on");
+    } else {
+        url = this.offUrl;
+        body = this.offBody;
+        this.log("Setting power state to off");
+    }
+
+    this.httpRequest(url, body, this.httpMethod, function (error, response, responseBody) {
+        if (error) {
+            this.log('HTTP set power function failed: %s', error.message);
+            callback(error);
+        }
+        else {
+            this.log('HTTP set power function succeeded!');
+            callback();
+        }
+    }.bind(this));
+};
+
+
+HttpSwitchAccessory.prototype.getServices = function () {
+    return [this.services.AccessoryInformation, this.services.Switch];
 };
