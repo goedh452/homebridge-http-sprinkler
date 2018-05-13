94c97b2e152a0e86650ee170d83997ab75db4f6b
goedh452 <bg.goedhart@gmail.com>
2018-05-13 11:45:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,101 +15,104 @@ function HttpSprinkler(log, config) {
 	
 	// Get config info
 	this.name                   = config["name"]          	|| "HTTP Switch";
-	this.checkStatus 	        = config["checkStatus"] 	|| "no";
-    this.pollingMillis          = config["pollingMillis"]   || 10000;
-    this.onUrl                  = config["onUrl"];
-    this.onBody                 = config["onBody"];
-    this.offUrl                 = config["offUrl"];
-    this.offBody                = config["offBody"];
-    this.statusUrl              = config["statusUrl"];
-    this.statusRegex            = config["statusRegex"]		|| "";
-    this.httpMethod             = config["httpMethod"] 	  	|| "GET";
+	this.checkStatus 	    = config["checkStatus"]	|| "no";
+	this.pollingMillis          = config["pollingMillis"]   || 10000;
+	this.onUrl                  = config["onUrl"]
+	this.onBody                 = config["onBody"];
+	this.offUrl                 = config["offUrl"];
+	this.offBody                = config["offBody"];
+	this.statusUrl              = config["statusUrl"];
+	this.statusRegex            = config["statusRegex"]	|| "";
+	this.httpMethod             = config["httpMethod"]   	|| "GET";
 
-    this.state = false;
+	this.state = false;
 
-    var that = this;
+	var that = this;
 
-	
+
 HttpSprinkler.prototype = {
 	
 	httpRequest: function (url, body, method, callback) {
+		
+		var callbackMethod = callback;
 
-    var callbackMethod = callback;
-
-    request({
-            url: url,
-            body: body,
-            method: method,
-            rejectUnauthorized: false
-        },
-        function (error, response, responseBody) {
-            if (callbackMethod) {
-                callbackMethod(error, response, responseBody)
-            }
-            else {
-                this.log.warn("callbackMethod not defined!");
-            }
-        })
+		request({
+			url: url,
+			body: body,
+			method: method,
+			rejectUnauthorized: false
+        	},
+        
+		function (error, response, responseBody) {
+			if (callbackMethod) {
+				callbackMethod(error, response, responseBody)
+			}
+			else {
+				this.log.warn("callbackMethod not defined!");
+			}
+		})
 	},
+	
 	
 	getPowerState: function (callback) {
 		var default_state_off = false
-        callback(null, !default_state_off);
-    },
+		callback(null, !default_state_off);
+	},
+	
 	
 	setPowerState: function (powerOn, callback) {
-
-    var url;
-    var body;
-	var inuse;
-
-    if (!this.onUrl || !this.offUrl) {
-        this.log.warn("Ignoring request: No power url defined.");
-        callback(new Error("No power url defined."));
-        return;
-    }
-
-    if (powerOn) {
-        url = this.onUrl;
-        body = this.onBody;
-		inuse = 1;
-        this.log("Setting power state to on");
-    } else {
-        url = this.offUrl;
-        body = this.offBody;
-		inuse = 2;
-        this.log("Setting power state to off");
-    }
-
 		
-	this.httpRequest(url, body, this.httpMethod, function (error, response, responseBody) {
-        if (error) {
-            this.log('HTTP set power function failed: %s', error.message);
-            callback(error);
-        }
-        else {
-			valveService.getCharacteristic(Characteristic.InUse).updatevalue(inuse);
-            this.log('HTTP set power function succeeded!');
-            callback();
-        }
-    	}.bind(this));
+		var url;
+		var body;
+		var inuse;
+
+		if (!this.onUrl || !this.offUrl) {
+			this.log.warn("Ignoring request: No power url defined.");
+			callback(new Error("No power url defined."));
+			return;
+		}
+
+		if (powerOn) {
+			url = this.onUrl;
+			body = this.onBody;
+			inuse = 1;
+			this.log("Setting power state to on");
+		} else {
+			url = this.offUrl;
+			body = this.offBody;
+			inuse = 0;
+			this.log("Setting power state to off");
+		}
+
+		this.httpRequest(url, body, this.httpMethod, function (error, response, responseBody) {
+			if (error) {
+				this.log('HTTP set power function failed: %s', error.message);
+				callback(error);
+			}
+			else {
+				valveService.getCharacteristic(Characteristic.InUse).updatevalue(inuse);
+				this.log('HTTP set power function succeeded!');
+				callback();
+			}
+		}//.bind(this));
 	},
+	
 	
 	getServices: function () {
 		var informationService = new Service.AccessoryInformation();
 
-        informationService
-                .setCharacteristic(Characteristic.Manufacturer, "Sprinkler Manufacturer")
-                .setCharacteristic(Characteristic.Model, "Sprinkler Model")
-                .setCharacteristic(Characteristic.SerialNumber, "Sprinkler Serial Number");
+		informationService
+			.setCharacteristic(Characteristic.Manufacturer, "Sprinkler Manufacturer")
+			.setCharacteristic(Characteristic.Model, "Sprinkler Model")
+			.setCharacteristic(Characteristic.SerialNumber, "Sprinkler Serial Number");
 
-        valveService = new Service.Valve(this.name);
-        valveService
-				.getCharacteristic(Characteristic.ValveType).updateValue(1)
-                .getCharacteristic(Characteristic.Active)
-               // .on('get', this.getStatusState.bind(this))
-                .on('set', this.setPowerState.bind(this))
+		valveService = new Service.Valve(this.name);
+		valveService
+			.getCharacteristic(Characteristic.ValveType).updateValue(1)
+			.getCharacteristic(Characteristic.Active)
+			.on('set', this.setPowerState.bind(this))
+			// .on('get', this.getStatusState.bind(this))
 
-        return [valveService];
-    
+		return [valveService];
+	}
 };
