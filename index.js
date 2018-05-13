4bd132610691e4adae56cf33de51d0d1c6808377
goedh452 <bg.goedhart@gmail.com>
2018-05-13 14:35:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ HttpSprinkler.prototype = {
 			this.log("Setting power state to off");
 		}
 
-		var res = request(this.url, "", this.http_method, "", "", "", callback);
+		var res = request(this.httpMethod, this.url, {});
 		if(res.statusCode > 400) {
 			this.log('HTTP power function failed');
 			callback(error);
