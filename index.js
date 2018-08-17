aae077d730019245ef94edc5c9546d9136cc21ad
goedh452 <bg.goedhart@gmail.com>
2018-08-17 18:47:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -188,17 +188,16 @@ HttpSprinkler.prototype =
 			this.log("Setting power state to off");
 		}
 		
-		this.valveService.getCharacteristic(Characteristic.InUse).updateValue(inuse);
-		
 		this.httpRequest(url, "", "GET", function (error, response, body)
 		{
 			if (error)
 			{
 				that.log("HTTP set status function failed %s", error.message);
 			} 
-		})	
+		}.bind(this))	
 		
 		this.log("HTTP power function succeeded!");
+		this.valveService.getCharacteristic(Characteristic.InUse).updateValue(inuse);
 		
 	},
 	
