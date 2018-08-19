4598187408a5bda23c1073d3380c1f5976acc4b9
goedh452 <bg.goedhart@gmail.com>
2018-08-19 12:09:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -267,6 +267,7 @@ HttpSprinkler.prototype =
 		
 		this.valveService.getCharacteristic(Characteristic.ValveType).updateValue(this.icon);
 		this.valveService.getCharacteristic(Characteristic.IsConfigured).updateValue(0);
+		this.valveService.addCharacteristic(Characteristic.SetDuration).updateValue(this.minTime);
 
 		switch (this.checkStatus)
 		{
